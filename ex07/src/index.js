58282be164b33c07aa208e4cca42507724682a2b
SamraBA <bandicsamra@gmail.com>
2021-09-20 21:55:04 +0200

    Day08

---
+++ ex07/src/index.js
@@ -0,0 +1,24 @@
+
+// Only change code below this line
+
+var sum = 0;
+
+function addThree() {
+  sum = sum + 3;
+console.log("addThree: " + sum);
+}
+
+function addFive() {
+    sum = sum + 5;
+    console.log("addThree: " + sum);
+    }
+
+
+addThree();
+addFive();
+
+// Only change code above this line
+module.exports = {
+    addThree,
+    addFive
+};
