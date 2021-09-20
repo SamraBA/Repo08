905adbc784cdb222b8238bea5d845fea9989081c
SamraBA <90388771+SamraBA@users.noreply.github.com>
2021-09-21 00:08:57 +0200

    Update index.js

--- ex05/src/index.js
+++ ex05/src/index.js
@@ -3,10 +3,10 @@
 
     // Only change code below this line
 
-    var dress = "little dress"
+    var dress = "little dress";
 
     function myDress() {
-        var dress = "ball gown"
+        var dress = "ball gown";
    
     // Only change code above this line
     return dress;
